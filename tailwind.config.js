8e49e2fbbf8b904918f06a77b34b9194b58cace0
Muddhu Krishna Aithem <154971467+Muddu2024@users.noreply.github.com>
2024-07-05 08:43:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ['Satoshi', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
